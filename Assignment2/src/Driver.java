bdd4383663ef352c4086f6299f25271973b281a3
Darren-ODonnell <darren.odonnell001@gmail.com>
2021-02-28 14:11:03 +0000

    Final Commit

--- Assignment2/src/Driver.java
+++ Assignment2/src/Driver.java
@@ -1,75 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
-public class Driver {
 
-    public static void main(String[] args){
-        //processBuilder.command("cmd.exe", "/c", "<ping command> -n <number of times to ping><website>");
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("cmd.exe", "/c", "ping -n 10 github.com");
+public class Driver{
+    public static void main(String[] args) {
+        // pool of 4 threads
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(4);
+        List<Future<String>> futures = new ArrayList<>();
+        // creates 4 callables to be executed
+        for (int i = 0; i < 4; i++) {
+            TaskCall callable = new TaskCall();
 
-            //Trying to run, catching IO and Interrupted Exceptions
-            try {
-                //Starting Process
-                Process process = processBuilder.start();
-
-                //Uses the InputStreamReader to interpret the input stream in process
-                /**
-                 * InputStream is being blocked
-                 */
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-                String line;
-
-                //Prints the next line where there is still a next line to be printed
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-                //Prints the exit code if no errors occur
-                int exitCode = process.waitFor();
-                System.out.println("\nExited with error code : " + exitCode);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            //Gets the future String from the call() method
+            Future<String> future = executorService.submit(callable);
+            //Adds the future String for the current process to the list
+            futures.add(future);
 
         }
-
+        // getting tasks results
+        for(int i = 0; i < futures.size(); i++) {
+            try {
+                //Prints the output held in the futures list
+                System.out.println(futures.get(i).get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        // shuts down the executor service
+        executorService.shutdown();
+    }
 }
-//2 classes are needed, in main line create an executor service, create a process builder and pass in the command, call the start on that. "Building on Q1"
-//Use executor service to start off a separate thread which will complete the task that is blocking
-//The second class will implement the callable interface, this will have a method "call()" which does what you did in question 1 where you did what you did to read the files
-//Read line and put into a string, add this string to the List, return list (call method returns list of Strings
-
-//The implements Callable<> inside the "<>" you put the return type you wish to return from the call method
-class Caller implements Callable<List<String>>{
-
-    //HOW to call this new Caller(process.getInputStream())
-    public Caller(){
-
-    }
+class TaskCall implements Callable<String> {
     @Override
-    /**
-     * Cannot return a List<String>
-     */
+    public String call() throws InterruptedException {
+        //str contains the output from each process
+        StringBuilder str = new StringBuilder();
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        //Sets the command to ping github.com 10 times per process
+        processBuilder.command("cmd.exe", "/c", "ping -n 10 github.com");
 
+        //Trying to run, catching IO and Interrupted Exceptions
+        //Starting Process
+        Process process = null;
+        try {
+            process = processBuilder.start();
 
-    //method -> call -> Read inputStream like Q1 -> return list of string(Strings from inputStream)
-    public List<String> call() throws Exception {
-        List<String> list = new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        if(process != null) {
+            //Initialise BufferedReader and InputStreamReader to process the input stream
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = "";
 
-        return null;
+            try {
+                //This appends the lines to hold the entire process in one StringBuilder
+                while ((line = reader.readLine()) != null) {
+                    str.append(line + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //Returns the StringBuilder as a string so that the future object can use it in main
+        return str.toString();
     }
-
-    //in main
-    //REturn list of strings into future object
-    //CAll get on the future objects -> List of strings, loop through this and print it out
 }
